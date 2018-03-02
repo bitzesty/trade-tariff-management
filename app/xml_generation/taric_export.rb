bda4cfdd79e9258310462f3734c28cd4ea5292b5
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-03-02 14:54:57 +0300

    [XML generation] added helpers and export task

---
+++ app/xml_generation/taric_export.rb
@@ -0,0 +1,14 @@
+module XmlGeneration
+  class TaricExport
+
+    attr_accessor :export_date
+
+    def initialize(export_date)
+      @export_date = export_date
+    end
+
+    def run
+
+    end
+  end
+end
