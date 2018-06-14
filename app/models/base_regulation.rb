9e69ad4852ca3ae5c6ceab050befcb50f45155e1
Mario Andrés Correa <a@macool.me>
2018-06-13 22:51:55 -0600

    do not set primary keys
    (fixes specs)

--- app/models/base_regulation.rb
+++ app/models/base_regulation.rb
@@ -14,13 +14,9 @@ class BaseRegulation < Sequel::Model
   include ::FormApiHelpers::RegulationSearch
 
   one_to_one :complete_abrogation_regulation,
-             primary_key: [ :complete_abrogation_regulation_id,
-                    :complete_abrogation_regulation_role ],
              key: [ :complete_abrogation_regulation_id,
                     :complete_abrogation_regulation_role ]
   one_to_one :explicit_abrogation_regulation,
-             primary_key: [ :explicit_abrogation_regulation_id,
-                    :explicit_abrogation_regulation_role ],
              key: [ :explicit_abrogation_regulation_id,
                     :explicit_abrogation_regulation_role ]
 
