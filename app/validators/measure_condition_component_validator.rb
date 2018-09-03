6f7c22990b1fe47d42ff0c15799a02437714e04a
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-09-03 21:06:19 +0300

    [CONFORMANCE RULES] ME106: The VP of the duty expression must span the VP of the measure.
    [TRELLO CARD](https://trello.com/c/XXm5AXbN/199-dit-ensure-exported-xml-conforms-to-ecdp-business-rules-phase-1-170)

--- app/validators/measure_condition_component_validator.rb
+++ app/validators/measure_condition_component_validator.rb
@@ -1,6 +1,11 @@
 class MeasureConditionComponentValidator < TradeTariffBackend::Validator
 
-  validation :ME105, 'The reference duty expression must exist.', on: [:create, :update] do
-    validates :presence, of: :duty_expression_id
+  validation :ME105, 'The reference duty expression must exist.', on: [:create, :update] do |record|
+    record.duty_expression_id.present? &&
+    record.duty_expression.present?
+  end
+
+  validation :ME106, 'The VP of the duty expression must span the VP of the measure.', on: [:create, :update] do
+    validates :validity_date_span, of: :duty_expression
   end
 end
--- spec/models/measure_condition_component_spec.rb
+++ spec/models/measure_condition_component_spec.rb
@@ -25,29 +25,45 @@ describe MeasureConditionComponent do
         let(:measurement_unit_qualifier_code) { Forgery(:basic).text(exactly: 1) }
       end
     end
+  end
 
-    describe 'Conformance rules' do
-      describe 'ME105' do
-        describe "The reference duty expression must exist" do
-          let!(:measure_condition_component) do
-            create(:measure_condition_component,
-                   duty_expression_id: DutyExpression::MEURSING_DUTY_EXPRESSION_IDS.sample
-            )
-          end
+  describe 'Conformance rules' do
+    let!(:measure)           { create :measure }
+    let!(:measure_condition) { create :measure_condition, measure_sid: measure.measure_sid }
 
-          it "valid" do
-            expect(measure_condition_component).to be_conformant
-          end
+    let(:duty_expression_id) { DutyExpression::MEURSING_DUTY_EXPRESSION_IDS.sample }
 
-          it "invalid" do
-            measure_condition_component.duty_expression_id = nil
-            measure_condition_component.save
+    let!(:duty_expression)   do
+      create :duty_expression,
+      duty_expression_id: duty_expression_id
+    end
 
-            expect(measure_condition_component).to_not be_conformant
-            expect(measure_condition_component.conformance_errors).to have_key(:ME105)
-          end
-        end
-      end
+    let!(:measure_condition_component) do
+      create(:measure_condition_component,
+             measure_condition_sid: measure_condition.measure_condition_sid,
+             duty_expression_id: duty_expression.duty_expression_id
+      )
+    end
+
+    it "valid" do
+      expect(measure_condition_component).to be_conformant
+    end
+
+    it "ME105: The reference duty expression must exist" do
+      measure_condition_component.duty_expression_id = nil
+      measure_condition_component.save
+
+      expect(measure_condition_component).to_not be_conformant
+      expect(measure_condition_component.conformance_errors).to have_key(:ME105)
+    end
+
+    it "ME106: The VP of the duty expression must span the VP of the measure." do
+      measure.validity_start_date = Date.today.ago(5.years)
+      measure.validity_end_date = Date.today.ago(4.years)
+      measure.save
+
+      expect(measure_condition_component).to_not be_conformant
+      expect(measure_condition_component.conformance_errors).to have_key(:ME106)
     end
   end
 end
