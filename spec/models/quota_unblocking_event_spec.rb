8ac01d71fd68a503e79c68a72b9f45d65e43ccac
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-10-05 12:46:57 +0530

    [CONFORMANCE RULES] QUBLE1 rule
    - Added specs for this rule

---
+++ spec/models/quota_unblocking_event_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe QuotaUnblockingEvent do
+  describe "validations" do
+    describe "conformance rules" do
+      let!(:quota_unblocking_event) { create :quota_unblocking_event }
+
+      describe "QUBLE1: The quota definition SID must exist." do
+        it "should run validation succssfully" do
+          expect(quota_unblocking_event).to be_conformant
+        end
+
+        it "should not run validation succssfully" do
+          quota_unblocking_event.quota_definition_sid = nil
+
+          expect(quota_unblocking_event).to_not be_conformant
+          expect(quota_unblocking_event.conformance_errors).to have_key(:QUBLE1)
+        end
+      end
+    end
+  end
+end
