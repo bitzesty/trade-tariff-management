79baf936adcc9b5db0a960a5ae0b1fcebe96f7fa
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-10-05 13:15:57 +0530

    [CONFORMANCE RULES] QRE1 rule
    - Added specs for this rule

---
+++ spec/models/quota_reopening_event_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe QuotaReopeningEvent do
+  describe "validations" do
+    describe "conformance rules" do
+      let!(:quota_reopening_event) { create :quota_reopening_event }
+
+      describe "QEE1: The quota definition SID must exist." do
+        it "should run validation succssfully" do
+          expect(quota_reopening_event).to be_conformant
+        end
+
+        it "should not run validation succssfully" do
+          quota_reopening_event.quota_definition_sid = nil
+
+          expect(quota_reopening_event).to_not be_conformant
+          expect(quota_reopening_event.conformance_errors).to have_key(:QRE1)
+        end
+      end
+    end
+  end
+end
