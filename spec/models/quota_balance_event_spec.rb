43ce6838c6c86803ec070ecb31d6148bc437b167
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-10-05 12:29:05 +0530

    [CONFORMANCE RULES] QBAE1 rule
    - Added specs for this rule

--- spec/models/quota_balance_event_spec.rb
+++ spec/models/quota_balance_event_spec.rb
@@ -7,7 +7,7 @@ describe QuotaBalanceEvent do
     let!(:balance_event2)    { create :quota_balance_event,
                                       occurrence_timestamp: 1.days.ago }
     let!(:balance_event3)    { create :quota_balance_event,
-                                      occurrence_timestamp: 2.days.ago }
+                                      occurrence_timestamp: 6.days.ago }
 
     it 'should order items by desc occurrence_timestamp' do
       expect(described_class.last).to eq(balance_event2)
@@ -17,6 +17,26 @@ describe QuotaBalanceEvent do
   describe '.status' do
     it "should return 'open' string" do
       expect(described_class.status).to eq('open')
+    end
+  end
+
+  describe 'validations' do
+    describe 'conformance rules' do
+      let!(:balance_event) { create :quota_balance_event,
+                             occurrence_timestamp: 3.days.ago }
+
+      describe "QBAE1: The quota definition SID must exist." do
+        it "should run validation succssfully" do
+          expect(balance_event).to be_conformant
+        end
+
+        it "should not run validation succssfully" do
+          balance_event.quota_definition_sid = nil
+
+          expect(balance_event).to_not be_conformant
+          expect(balance_event.conformance_errors).to have_key(:QBAE1)
+        end
+      end
     end
   end
 end
