fb34004946ecf5c925b0c9d6d582b297a074ed3c
Alexey Klimuk <alexey.klimuk@bitzesty.com>
2019-02-13 07:53:49 +0100

    xml export: add exchange rates to national xml

--- app/models/xml_generation/db_search.rb
+++ app/models/xml_generation/db_search.rb
@@ -117,16 +117,24 @@ module XmlGeneration
   private
 
     def data
-      [Measure].map do |entity|
-        "#{entity}::Operation".constantize.where(where_clause).all
+      [MonetaryExchangePeriod, MonetaryExchangeRate, Measure].map do |entity|
+        "#{entity}::Operation".constantize.where(where_clause(entity)).all
       end.flatten
     end
 
-    def where_clause
+    def where_clause(klass)
       if end_date.present?
-        ['? <= operation_date AND operation_date <= ? AND national = ?', start_date, end_date, true]
+        if klass.columns.include?(:national)
+          ['? <= operation_date AND operation_date <= ? AND national = ?', start_date, end_date, true]
+        else
+          ['? <= operation_date AND operation_date <= ?', start_date, end_date]
+        end
       else
-        ['operation_date >= ? AND national = ?', start_date, true]
+        if klass.columns.include?(:national)
+          ['operation_date >= ? AND national = ?', start_date, true]
+        else
+          ['operation_date >= ?', start_date]
+        end
       end
     end
   end
