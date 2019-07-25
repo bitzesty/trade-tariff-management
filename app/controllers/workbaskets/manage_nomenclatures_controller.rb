904782218197bcede49571926e6e768556ba37dd
Steve Holmes <steholmes@gmail.com>
2019-07-25 15:54:47 +0100

    Fix: only display description in nomenclature tree if description published
    Prior to this change, the tree displayed descriptions even if they were
    waiting for approval.
    
    This change checks the status for published.

--- app/controllers/workbaskets/manage_nomenclatures_controller.rb
+++ app/controllers/workbaskets/manage_nomenclatures_controller.rb
@@ -33,11 +33,14 @@ module Workbaskets
       )
 
       if workbasket.save
+        original_nomenclature = find_original_nomenclature(workbasket_params[:original_nomenclature])
         workbasket.settings.update(
           workbasket_id: workbasket.id,
           workbasket_name: workbasket_params[:workbasket_name],
           reason_for_changes: workbasket_params[:reason_for_changes],
-          original_nomenclature: find_original_nomenclature(workbasket_params[:original_nomenclature]).goods_nomenclature_sid
+          validity_start_date: extract_date_from_params(workbasket_params),
+          original_nomenclature: original_nomenclature.goods_nomenclature_sid,
+          original_description: original_nomenclature.description
         )
       end
       workbasket
